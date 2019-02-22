1ca629da33311f673c6383e7d170ce164cbf510a
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-22 15:18:44 +0700

    Add get users functionality

--- index.js
+++ index.js
@@ -10,15 +10,35 @@ require('dotenv').config()
 var mongoose = require('mongoose')
 
 //Set up default mongoose connection
-var mongoDB = process.env.DB_PATH
-mongoose.connect(mongoDB, { useNewUrlParser: true })
+mongoose.connect(`${process.env.URL}/${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+})
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise
 //Get the default connection
-var db = mongoose.connection
+const db = mongoose.connection
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// User model => users collection
+const User = mongoose.model('User', {
+  name: String,
+  email: String,
+})
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World',
+  })
+})
+
+app.get('/users', async (req, res) => {
+  res.send({
+    message: 'List of all users',
+    users: await User.find(),
+  })
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port} `)
