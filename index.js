c377266910dcae73673db3f080baf94f8fd3d55c
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-22 14:46:30 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// EXPRESS
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8000
+
+//Import the mongoose module
+var mongoose = require('mongoose')
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost:27017/mytasks'
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise
+//Get the default connection
+var db = mongoose.connection
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} `)
+})
